a8dbb5e681087c5a672663f025556e01aa8482ae
adamdeere <adam.deere@hotmail.com>
2020-09-10 11:52:38 +0100

    last commit before lunch

--- ObjectManagementTut/Assets/Scripts/Object script/KillZone.cs
+++ ObjectManagementTut/Assets/Scripts/Object script/KillZone.cs
@@ -5,15 +5,33 @@ namespace Object_script
     public class KillZone : MonoBehaviour
     {
         [SerializeField] private float dyingDuration;
+        
+        void OnDrawGizmos () 
+        {
+            Gizmos.color = new Color(0.28f, 1f, 0.19f);
+            Gizmos.matrix = transform.localToWorldMatrix;
+            var c = GetComponent<Collider>();
+            var b = c as BoxCollider;
+            if (b != null) 
+            {
+                Gizmos.DrawWireCube(b.center, b.size);
+                return;
+            }
+            var s = c as SphereCollider;
+            if (s == null) return;
+            Gizmos.DrawWireSphere(s.center, s.radius);
+            return;
+        }
+        
         public void OnTriggerEnter (Collider other) 
         {
-            IKillable shapeComponent = other.GetComponent<IKillable>();
-            Shape shape = other.GetComponent<Shape>();
+            var shapeComponent = other.GetComponent<IKillable>();
+            var shape = other.GetComponent<Shape>().IsMarkedAsDying;
             
             if (dyingDuration <= 0f) 
                 shapeComponent?.Kill();
             
-            else if (!shape.IsMarkedAsDying)
+            else if (!shape)
                 shapeComponent?.AddInterface(dyingDuration);
             
         }
--- ObjectManagementTut/Assets/Scripts/Object script/LifeZone.cs
+++ ObjectManagementTut/Assets/Scripts/Object script/LifeZone.cs
@@ -5,16 +5,35 @@ namespace Object_script
 {
     public class LifeZone : MonoBehaviour
     {
+        void OnDrawGizmos () 
+        {
+            Gizmos.color = new Color(1f, 0.39f, 0.55f);
+            Gizmos.matrix = transform.localToWorldMatrix;
+            var c = GetComponent<Collider>();
+            var b = c as BoxCollider;
+            if (b != null) 
+            {
+                Gizmos.DrawWireCube(b.center, b.size);
+                return;
+            }
+            var s = c as SphereCollider;
+            if (s != null) 
+            {
+                Gizmos.DrawWireSphere(s.center, s.radius);
+                return;
+            }
+        }
+        
         [SerializeField] private float dyingDuration;
         void OnTriggerExit (Collider other)
         {
             var shapeComponent = other.GetComponent<IKillable>();
-            var shape = other.GetComponent<Shape>();
+            var shape = other.GetComponent<Shape>().IsMarkedAsDying;
             
             if (dyingDuration <= 0f) 
                 shapeComponent?.Kill();
             
-            else if (!shape.IsMarkedAsDying)
+            else if (!shape)
                 shapeComponent?.AddInterface(dyingDuration);
             
         }
